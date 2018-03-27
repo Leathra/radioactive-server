a6a7bf191ad86e4e875d44f7c8e957da09b3fc97
leathra schmitt-evans <leathraschmitt-evans@Leathras-imac.local>
2018-03-27 00:26:00 -0600

    auth flow

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'Mom!' });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 //look at underlining enviroment and see if they have specified a port to use when running in production
 //put boolian statment for development enviroment.
