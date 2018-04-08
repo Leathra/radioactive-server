ea98e8f31768d4e0ef7d29007d2edfbc801bb8ee
leathra schmitt-evans <leathraschmitt-evans@Leathras-imac.network.tds.net>
2018-04-07 23:18:43 -0600

    Billing and client side app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Page Title</h1>
+      <p>Page content</p>
+    </div>
+  );
+};
+
+export default Landing;
